15b0025e65c79c42c967066411d769c0181734d1
Krakozyaka <154098921+Krakozyaka@users.noreply.github.com>
2023-12-19 22:47:30 +0300

    Add files via upload

---
+++ lecture1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int var_calculation(string name, int variant) {
+	return (char(name[0]) % variant);
+}
+
+int main()
+{
+	string name;
+	int variant, res;
+	cin >> name >> variant;
+	res = var_calculation(name, variant);
+	cout << res << endl;
+	return 0;
+}

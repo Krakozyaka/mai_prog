591eaa262c23920fa4d4fd02d910a7f048c553a2
Krakozyaka <154098921+Krakozyaka@users.noreply.github.com>
2023-12-25 00:29:10 +0300

    Add files via upload

---
+++ lecture7/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <mutex>
+#include <thread>
+
+using namespace std;
+
+class Multithread {
+private:
+	int cnt;
+public: 
+	bool flag = true;
+	Multithread() {
+		cnt = 0;
+	}
+	void increment() {
+        for (int i = 0; i < 100; i++) {
+            cnt++;
+			this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+		flag = false;
+	}
+	void print() {
+		while (flag){
+			cout << cnt << endl;
+			this_thread::sleep_for(std::chrono::milliseconds(5));
+		}
+	}
+	
+};
+
+int main() {
+	Multithread a;
+	thread incr([&a]() {a.increment(); });
+	thread printing([&a]() {a.print(); });
+	incr.join();
+	printing.join();
+	return 0;
+}
